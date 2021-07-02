013c16ae3dd1c6f74570e2cb0606229e22fcf61c
José Hidalgo <jhidalgoc@gmail.com>
2021-07-02 21:59:46 +0000

    Change path movies_app

--- app/MvcMovie/Startup.cs
+++ app/MvcMovie/Startup.cs
@@ -43,6 +43,8 @@ namespace MvcMovie
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+
+		app.UsePathBase("/movies");
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
